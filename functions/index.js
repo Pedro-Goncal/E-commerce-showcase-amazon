481d10475dee0e575c70732a30c28ff5aa3327c0
Pedro-Goncal <bpg.pedro@gmail.com>
2020-10-02 09:17:26 -0500

    API for stripe updated

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-  "sk_test_51HPvU9DFg5koCdLGeOEiFvwHat4v8eMjX6SY0YCwxPBQBUPhKy1fPVhiSM5cQtgW7QBG9ydQcXnW57TDxVE2f3H000HSfmEQZF"
+  "sk_test_51HQDhlKMuIhlMruhkaUqf6ELXHGCbxdOnSTkKYA8QiOy6YP3kFzubeOdQea9UyYAmB9ei0mp9I3y6OQyCStRFfBt000F6u8yGh"
 );
 
 // API
