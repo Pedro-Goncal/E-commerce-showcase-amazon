e50ae401982460f12e9b0674e5ed4bf80d48379b
Pedro Goncalves <bpg.pedro@gmail.com>
2020-12-17 12:42:11 -0600

    Updates

---
+++ precache-manifest.77b7b94aa98c0505b6b0be4f760c3600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66060e80ff268df67f5d0b8eb51ff4b6",
+    "url": "/E-commerce-showcase-amazon/index.html"
+  },
+  {
+    "revision": "f624c9879ac122b4231b",
+    "url": "/E-commerce-showcase-amazon/static/css/main.ff76810c.chunk.css"
+  },
+  {
+    "revision": "13eaf145df8688a00910",
+    "url": "/E-commerce-showcase-amazon/static/js/2.5cef577e.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/E-commerce-showcase-amazon/static/js/2.5cef577e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f624c9879ac122b4231b",
+    "url": "/E-commerce-showcase-amazon/static/js/main.893ecaf8.chunk.js"
+  },
+  {
+    "revision": "17ea2f1d5433fa522521",
+    "url": "/E-commerce-showcase-amazon/static/js/runtime-main.800d9512.js"
+  },
+  {
+    "revision": "533423e1418dfd15cd7eaeece58fcfe0",
+    "url": "/E-commerce-showcase-amazon/static/media/emptycart.533423e1.png"
+  }
+]);
